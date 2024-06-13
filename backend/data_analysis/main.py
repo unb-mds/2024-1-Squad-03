31cc8e860baca67a25bbeed49d7ec116297e1cac
Marcelo-Adrian <marcelo.10vip@gmail.com>
2024-06-13 20:30:17 -0300

    Fix: Banco de dados completo

--- backend/data_analysis/main.py
+++ backend/data_analysis/main.py
@@ -1,24 +1,24 @@
 # -*- coding: latin1 -*-
 import csv
-import csv
 import json
 import os
-import os
 
-def analyse_licitacoes(file_path):
-    data=[]
+def analyse_licitacoes(file_path, selected_columns):
+    data = []
     try:
         with open(file_path, 'r', encoding='latin1') as file:
             csv_reader = csv.DictReader(file, delimiter=';')
             for line in csv_reader:
-                if line['Município'] == "BRASILIA" and line['Modalidade Compra'] == "Pregão - Registro de Preço" and line['Valor Licitação'] != "0,00":
-                    data.append(line)
+                if (line['Município'] == "BRASILIA" and 
+                    line['Modalidade Compra'] == "Pregão - Registro de Preço" and 
+                    line['Valor Licitação'] != "0,00"):
+                    
+                    filtered_line = {key: line[key] for key in selected_columns}
+                    data.append(filtered_line)
     except FileNotFoundError:
         print("The file does not exist.")
     return data
 
-def save_json(data):
-    json_file = "output.json"
 def save_json(data):
     json_file = "output.json"
     if data:
@@ -30,12 +30,20 @@ def save_json(data):
 
 def main():
     folder_path = "licitacoes"  
+    selected_columns = [
+        "Número Licitação", "Nome UG", 
+        "Modalidade Compra", 
+        "Número Processo", "Objeto", "Situação Licitação", 
+        "Nome Órgão Superior", 
+        "Nome Órgão", "UF", "Município", 
+        "Data Resultado Compra", "Data Abertura", "Valor Licitação"
+    ]
+
     for filename in os.listdir(folder_path):
         if filename.endswith(".csv"):
             file_path = os.path.join(folder_path, filename)
-        data = analyse_licitacoes(file_path)
-        save_json(data)
-
+            data = analyse_licitacoes(file_path, selected_columns)
+            save_json(data)
 
 if __name__ == "__main__":
     main()
